3a09e38cd62f017c1fefaedd8d020db6f9312695
saparadox <52182338+saparadox@users.noreply.github.com>
2024-05-30 17:17:01 -0400

    Multiples solution

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Multiples {
+
+public class Multiples {
+    public static void main(String[] args) {
+        int i = 1000;
+        int j = 0;
+        while (i >= 3){
+            i--;
+            if(i % 3 == 0){
+                j++;
+            }
+            if(i % 5 == 0){
+                j++;
+            }
+        }
+        System.out.println("Multiples: " + j);
+    }
 }
